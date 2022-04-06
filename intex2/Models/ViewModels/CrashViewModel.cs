aa43e91380e266cb16811d64e2e3acd1461f7970
logankimball <logankimbs@gmail.com>
2022-04-05 18:31:49 -0600

    added pagination to table

---
+++ intex2/Models/ViewModels/CrashViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+
+namespace intex2.Models.ViewModels
+{
+    public class CrashViewModel
+    {
+        public IQueryable<CrashModel> Crashes { get; set; }
+        public PageInformation PageInfo { get; set; }
+    }
+}
